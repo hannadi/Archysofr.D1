14a0d2a2cb2c5806de58eb22b6ce71589ff4c9ca
Hanna Dimova <anna.dimova15@gmail.com>
2019-01-23 13:51:01 +0200

    #1: Add BusinessException.

---
+++ Archysoft.D1.Model/Exceptions/BusinessException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Archysoft.D1.Model.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        { }
+        public BusinessException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
--- Archysoft.D1.Model/Services/Concrete/AuthService.cs
+++ Archysoft.D1.Model/Services/Concrete/AuthService.cs
@@ -1,13 +1,34 @@
-﻿using Archysoft.D1.Model.Auth;
+﻿using Archysoft.D1.Data.Entities;
+using Archysoft.D1.Model.Auth;
+using Archysoft.D1.Model.Exceptions;
+using Archysoft.D1.Model.Repositories.Abstract;
 using Archysoft.D1.Model.Services.Abstract;
 
 namespace Archysoft.D1.Model.Services.Concrete
 {
     public class AuthService: IAuthService
     {
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public TokenModel Login(LoginModel model)
         {
-            return null;
+            var user = _userRepository.Get(model.Login, model.Password);
+            if (user == null)
+            {
+                throw new BusinessException("Invalid User");
+            }
+
+            return GenerateToken(user);
+        }
+
+        private TokenModel GenerateToken(User user)
+        {
+            return new TokenModel();
         }
     }
 }
