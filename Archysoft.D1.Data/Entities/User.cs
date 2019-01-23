10c13f15b99d9229a9f73bba79b73590eb8fc6e9
Hanna Dimova <anna.dimova15@gmail.com>
2019-01-23 11:38:08 +0200

    #1: Add User Entity and AuthController.

---
+++ Archysoft.D1.Data/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Archysoft.D1.Data.Entities
+{
+    public class User: IdentityUser<Guid>
+    {
+    }
+}
---
+++ Archysoft.D1.Web.Api/Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Archysoft.D1.Web.Api.Controllers
+{
+    public class AuthController: Controller
+    {
+    }
+}
